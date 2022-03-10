d6248086d10d3647e47375dcef031c873cfa1605
Breno Albuquerque <brenoralbu@hotmail.com>
2022-03-10 13:44:45 -0300

    Grupo 9 - Requisito 4

--- src/pages/TelaDoJogo.js
+++ src/pages/TelaDoJogo.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import Feedback from './Feedback';
-// import { connect } from 'react-redux';
+import Feedback from './Feedback';
+//  import { connect } from 'react-redux';
 
 class TelaDoJogo extends Component {
   render() {
     return (
       <div>
-        tela do jogo
+        <Feedback />
       </div>
     );
   }
